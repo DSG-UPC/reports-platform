0851ed2d6f3f52318a66e76f9af183cf4b7c1669
Nil Quera <nlquera97@gmail.com>
2020-12-22 12:22:28 +0100

    Change hash algorithm to SHA-3

--- src/pages/Stamps/CreateStamps/UploadReport.js
+++ src/pages/Stamps/CreateStamps/UploadReport.js
@@ -1,16 +1,17 @@
-import { Keccak } from "sha3"
+import { SHA3 } from "sha3"
 import { FileInput } from "components"
 
 export default function UploadReport({ hash, setHash }) {
   const handleChange = async (evt) => {
     if (evt.target.files[0]?.type === "text/csv") {
       const file = evt.target.files[0]
-      const keccak = new Keccak(256)
+      const sha3 = new SHA3(256)
       const reader = new FileReader()
       reader.onload = (e) => {
         const content = e.target.result
-        keccak.update(content)
-        const hash = keccak.digest("hex")
+        console.log(content)
+        sha3.update(content)
+        const hash = sha3.digest("hex")
         setHash(hash)
       }
       reader.readAsText(file)
